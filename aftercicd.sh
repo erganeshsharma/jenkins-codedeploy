a0884c5e95452c3ea05fc69f80ed570d0f5e10cd
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-09-11 13:19:03 +0530

    update aftercicd.sh

--- aftercicd.sh
+++ aftercicd.sh
@@ -1,1 +1,1 @@
-echo "This script will contain commands that will run some test cases to check if the application is deployed properly."
+cp /var/lib/jenkins/workspace/jenkins-codedeploy/* /var/www/html/
