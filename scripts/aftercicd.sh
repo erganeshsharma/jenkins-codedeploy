d0f145a7257c2a9e76a091879375095784345fcb
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-09-14 14:50:15 +0530

    update beforecicd.sh and aftercicd.sh

--- scripts/aftercicd.sh
+++ scripts/aftercicd.sh
@@ -5,7 +5,7 @@ else
   export STATUS="failure"
 fi
 
-curl "https://api.github.com/repos/erganeshsharma/jenkins-codedeploy/statuses/$GIT_COMMIT?access_token=abc123" \
+curl "https://api.github.com/repos/erganeshsharma/jenkins-codedeploy/statuses/$GIT_COMMIT?access_token=b59f4a5d92ec6d3175fb785734398d6b36fbf08d" \
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"state\": \"pending\", \"description\": \"Jenkins\", \"target_url\": \"http://52.34.145.34:8080/job/jenkins-codedeploy/$BUILD_NUMBER/console\"}"
