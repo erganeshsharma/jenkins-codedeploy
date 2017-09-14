56df9bc9019656c01350b22f18a819a2610108ae
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-09-14 13:30:04 +0530

    update aftercicd.sh

--- scripts/aftercicd.sh
+++ scripts/aftercicd.sh
@@ -1,1 +1,11 @@
-echo "Commands and scripts to be run after CI/CD."
+if [[ $BUILD_STATUS == "success" ]]
+then
+  export STATUS="success"
+else
+  export STATUS="failure"
+fi
+
+curl "https://api.github.com/repos/erganeshsharma/jenkins-codedeploy/statuses/$GIT_COMMIT?access_token=abc123" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"pending\", \"description\": \"Jenkins\", \"target_url\": \"http://52.34.145.34:8080/job/jenkins-codedeploy/$BUILD_NUMBER/console\"}"
