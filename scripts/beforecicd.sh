c9b64e92296c8cab683a914a24273066bbfe2d70
[erganeshsharma] <[ganeshsharmait@gmail.com]>
2017-09-14 13:29:33 +0530

    update beforecicd.sh

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -1,1 +1,5 @@
-echo "Scripts to be run before CI/CD"
+curl "https://api.github.com/repos/erganeshsharma/jenkins-codedeploy/statuses/$GIT_COMMIT?access_token=abc123" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"pending\", \"description\": \"Jenkins\", \"target_url\": \"http://52.34.145.34:8080/job/jenkins-codedeploy/$BUILD_NUMBER/console\"
+}"
